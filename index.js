1d5c5f5f5dd4be5a690a690d5245253e3b63571a
Fomachka <fomachka93@yahoo.com>
2020-05-06 16:34:29 +0900

    final change

--- index.js
+++ index.js
@@ -5,22 +5,16 @@ var randomNumber3 = Math.floor(Math.random() * 6 + 1);
 function RandomImage(number){
     if(number === 1){
         document.querySelector("div img").setAttribute("src", "images/dice1.png");
-        document.querySelector("div .img2").setAttribute("src", "images/dice1.png");
     }else if(number === 2){
         document.querySelector("div img").setAttribute("src", "images/dice2.png");
-        document.querySelector("div .img2").setAttribute("src", "images/dice2.png");
     }else if(number === 3){
         document.querySelector("div img").setAttribute("src", "images/dice3.png");
-        document.querySelector("div .img2").setAttribute("src", "images/dice3.png");
     }else if(number === 4){
         document.querySelector("div img").setAttribute("src", "images/dice4.png");
-        document.querySelector("div .img2").setAttribute("src", "images/dice4.png");
     }else if(number === 5){
         document.querySelector("div img").setAttribute("src", "images/dice5.png");
-        document.querySelector("div .img2").setAttribute("src", "images/dice5.png");
     }else{
         document.querySelector("div img").setAttribute("src", "images/dice6.png");
-        document.querySelector("div .img2").setAttribute("src", "images/dice6.png");
     }
 
 }
