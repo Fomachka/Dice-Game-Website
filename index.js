d0c1ea084bb9204b5fe1056fca2d7669ce2b178e
Fomachka <fomachka93@yahoo.com>
2020-05-06 15:56:07 +0900

    initial

---
+++ index.js
@@ -0,0 +1,57 @@
+var randomNumber = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+
+function RandomImage(number){
+    if(number === 1){
+        document.querySelector("div img").setAttribute("src", "images/dice1.png");
+        document.querySelector("div .img2").setAttribute("src", "images/dice1.png");
+    }else if(number === 2){
+        document.querySelector("div img").setAttribute("src", "images/dice2.png");
+        document.querySelector("div .img2").setAttribute("src", "images/dice2.png");
+    }else if(number === 3){
+        document.querySelector("div img").setAttribute("src", "images/dice3.png");
+        document.querySelector("div .img2").setAttribute("src", "images/dice3.png");
+    }else if(number === 4){
+        document.querySelector("div img").setAttribute("src", "images/dice4.png");
+        document.querySelector("div .img2").setAttribute("src", "images/dice4.png");
+    }else if(number === 5){
+        document.querySelector("div img").setAttribute("src", "images/dice5.png");
+        document.querySelector("div .img2").setAttribute("src", "images/dice5.png");
+    }else{
+        document.querySelector("div img").setAttribute("src", "images/dice6.png");
+        document.querySelector("div .img2").setAttribute("src", "images/dice6.png");
+    }
+
+}
+function RandomImageTwo(number){
+    
+    if(number === 1){
+        document.querySelector("div .img2").setAttribute("src", "images/dice1.png");
+    }else if(number === 2){
+        document.querySelector("div .img2").setAttribute("src", "images/dice2.png");
+    }else if(number === 3){
+        document.querySelector("div .img2").setAttribute("src", "images/dice3.png");
+    }else if(number === 4){
+        document.querySelector("div .img2").setAttribute("src", "images/dice4.png");
+    }else if(number === 5){
+        document.querySelector("div .img2").setAttribute("src", "images/dice5.png");
+    }else{
+        document.querySelector("div .img2").setAttribute("src", "images/dice6.png");
+    }
+
+}
+
+function winner(){
+    if(randomNumber > randomNumber2){
+        document.querySelector("h1").textContent = "🚩Player1 Wins!";
+    }else if(randomNumber < randomNumber2){
+        document.querySelector("h1").textContent = "Player2 Wins! 🚩";
+    }else{
+        document.querySelector("h1").textContent = "💢Draw";
+                        
+    }
+}
+RandomImage(randomNumber);
+RandomImageTwo(randomNumber2);
+winner();
